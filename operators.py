c5f0c40028a4d298adb37fb4e5928c70a550cbb9
Syed shahriar ahmed(Shihab) <106880872+Shahriarshihab5@users.noreply.github.com>
2024-02-05 21:28:39 +0600

    new commit

---
+++ operators.py
@@ -0,0 +1,54 @@
+#additinn
+a= 5
+b= 2
+print(a+b)
+
+#substraaction
+
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#Division
+print(a/b)
+
+#modulus
+print(a%b)
+
+#Exponentiation(dui bar gun mane y er value joto toto bar x er value gun jemon 5**3 =5*5*5)
+
+x=5
+y=3
+print(x**y)
+#Floor Division
+sum1 = 15
+sum2 =2
+print(sum1/sum2)
+
+#ASSIGNMNET OPERATORS
+d=5
+sum=d+5
+print(sum)
+aa=10
+aa+=10
+print(aa)
+aaa=20
+aaa-=40
+print(aaa)
+
+#Comparision Operator
+xx=5
+xy=6
+print(x==y)
+xx=5
+xy=6
+print(x!=y)
+
+#swaping
+
+c = 50
+d=60
+print(c,d)
+c,d = d,c
+print(c,d)
