97182aa76e445278276d9975ef7031d2cc6af27d
Syed shahriar ahmed(Shihab) <106880872+Shahriarshihab5@users.noreply.github.com>
2024-02-05 21:18:00 +0600

    new commit

--- .symmetric_difference.py
+++
@@ -1,8 +0,0 @@
-a = int(input()) 
-b = set(map(int, input().split()))  
-
-e = int(input())  
-r = set(map(int, input().split()))  
-
-u = b.symmetric_difference(r) 
-print(len(u))  
--- Local and global varialble.py
+++
@@ -1,9 +0,0 @@
-a=30 #global scope/variable
-b=50
-
-def fn():
-    a=100 #local scope/variable
-    print(a)
-
-print(a)#printing the value of global variable
-fn()#printing the value of local variable
