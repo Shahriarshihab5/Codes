fd1a6eff5a99a84da9f1c454d6465ebb551f8db5
Syed shahriar ahmed(Shihab) <106880872+Shahriarshihab5@users.noreply.github.com>
2024-02-05 21:03:21 +0600

    Third commit

---
+++ Math functions.py
@@ -0,0 +1,18 @@
+import math
+a=min(10,20,30,40,50) #maximum
+print(a)
+b=max(50,100,70,80,90,10) #minimum
+print(b)
+
+k=abs(-1500)#absolute value
+print(k)
+
+#power of variable
+# x	A number, the base
+# y	A number, the exponent
+# z	Optional. A number, the modulus
+# pow(x, y, z)
+x=pow(4,2)
+print(x)
+u=pow(4,2,7)#Return the value of 4 to the power of 3, modulus 5 (same as (4 * 4 * 4) % 5):
+print(u)
