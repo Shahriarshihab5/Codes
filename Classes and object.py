f4dd6bf519897b86ce98e63a2258b9cee8453b44
Syed shahriar ahmed(Shihab) <106880872+Shahriarshihab5@users.noreply.github.com>
2024-02-05 21:30:49 +0600

    new commit

---
+++ Classes and object.py
@@ -0,0 +1,13 @@
+class Hablu:
+    Name=""
+    number=""
+
+a = Hablu()
+b=Hablu()
+c=Hablu()
+a.name="Shihab"
+b.name ="Shahriar"
+c.number=1737712021
+print(b.name)
+print(a.name)
+print(c.number)
