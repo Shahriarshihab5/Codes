531fcd83b2846ee570fe52596ce17327fbacd60e
Syed shahriar ahmed(Shihab) <106880872+Shahriarshihab5@users.noreply.github.com>
2024-02-18 02:41:28 +0600

    new commit

---
+++ Converting upper case and lower case .py
@@ -0,0 +1,7 @@
+t1=("Daffodil","International","University")
+print("Uppercase: ")
+for s in range(len(t1)):
+  print(t1[s].upper(),end=' ')
+print("\nLowercase: ")
+for s in range(len(t1)):
+  print(t1[s].lower(),end=' ')
